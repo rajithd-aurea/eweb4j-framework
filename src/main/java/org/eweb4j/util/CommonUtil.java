b5511bd3f28785f3fab7628364603630ad2db6ae
laiweiwei <l.weiwei@163.com>
2013-03-12 17:13:50 +0800

    fixed CommonUtil.toHTML() issue

--- src/main/java/org/eweb4j/util/CommonUtil.java
+++ src/main/java/org/eweb4j/util/CommonUtil.java
@@ -1246,7 +1246,7 @@ public class CommonUtil {
 		String s4 = s3.replace("&gt;", ">"); // 转换字符串中的">"符号
 		String s5 = s4.replace("<br />", "\t\n"); // 转换字符串中的回车换行
 		String s6 = s5.replace("&quot;", "\""); // 转换字符串中的"\""符号
-		String s7 = s5.replace("&apos;", "'"); // 转换字符串中的"'"符号
+		String s7 = s6.replace("&apos;", "'"); // 转换字符串中的"'"符号
 
 		return s7;
 	}
